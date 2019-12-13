de0a853f773be89bb37ac520c09b61707db05e78
Loowood <lorisg01@yahoo.fr>
2019-12-13 16:56:51 +0100

    Tous les canards + Spin

--- client/javascripts/game/arcand.js
+++ client/javascripts/game/arcand.js
@@ -1,10 +1,87 @@
 class Arcand {
 
     // Constructors
-    constructor(nb){
+    constructor(nb, texture){
         this.number = nb;
+        this.texture = texture;
         this.existence = true;
         this.chosenOne = false;
     }
-    
+
+
+    display (x, y) {
+        // push();
+        // texture(canard1_texture);
+        // rotateY(PI);
+        // box(160, 240, 8);
+        // pop();
+        // push();
+        // translate(0,0,8);
+        // texture(dos_texture);
+        // box(160, 240, 8);
+        // pop();
+        // push();
+        // translate(80.5,0,4);
+        // rotateY(PI/2);
+        // texture(coteVertical_texture);
+        // box(16, 240, 0.2);
+        // pop();
+        // push();
+        // translate(-80.5,0,4);
+        // rotateY(3 * PI/2);
+        // texture(coteVertical_texture);
+        // box(16, 240, 0.2);
+        // pop();
+        // push();
+        // translate(0,120.5,4);
+        // rotateX(PI/2);
+        // texture(coteHorizontal_texture);
+        // box(160, 16, 0.2);
+        // pop();
+        // push();
+        // translate(0,-120.5,4);
+        // rotateX(3 * PI/2);
+        // texture(coteHorizontal_texture);
+        // box(160, 16, 0.2);
+        // pop();
+        push();
+            translate(x, y);
+            //rotateX(frameCount * 0.01);
+            rotateY(frameCount * map(this.number, 1,9,0.1,0.15));
+            push();
+            texture(this.texture);
+            rotateY(PI);
+            box(160, 240, 8);
+            pop();
+            push();
+            translate(0,0,8);
+            texture(Arcand.dos_texture);
+            box(160, 240, 8);
+            pop();
+            push();
+            translate(80.5,0,4);
+            rotateY(PI/2);
+            texture(Arcand.coteVertical_texture);
+            box(16, 240, 0.2);
+            pop();
+            push();
+            translate(-80.5,0,4);
+            rotateY(3 * PI/2);
+            texture(Arcand.coteVertical_texture);
+            box(16, 240, 0.2);
+            pop();
+            push();
+            translate(0,120.5,4);
+            rotateX(PI/2);
+            texture(Arcand.coteHorizontal_texture);
+            box(160, 16, 0.2);
+            pop();
+            push();
+            translate(0,-120.5,4);
+            rotateX(3 * PI/2);
+            texture(Arcand.coteHorizontal_texture);
+            box(160, 16, 0.2);
+            pop();
+        pop();
+    }
 }
--- client/javascripts/game/sketch.js
+++ client/javascripts/game/sketch.js
@@ -1,57 +1,33 @@
-let a1 = new Arcand(1);
-let canard1_texture;
+let arcands = [];
+let arcands_textures = [];
 let dos_texture;
 let coteVertical_texture;
 let coteHorizontal_texture;
 function preload() {
-	canard1_texture = loadImage('../../assets/images/Canard 1.png');
+	for( let i = 0; i < 9; i ++ ) {
+		arcands_textures.push(loadImage('../../assets/images/Canard ' + (i+1).toString() + '.png'));
+	}
 	dos_texture = loadImage('../../assets/images/Dos.png');
 	coteVertical_texture = loadImage('../../assets/images/CotéVertical.png');
 	coteHorizontal_texture = loadImage('../../assets/images/CotéHorizontal.png');
+	Arcand.dos_texture = dos_texture;
+	Arcand.coteVertical_texture = coteVertical_texture;
+	Arcand.coteHorizontal_texture = coteHorizontal_texture;
 }
 
 function setup(){
     createCanvas(windowWidth, windowHeight, WEBGL);
+	for( let i = 0; i < 9; i ++ ) {
+		arcands.push(new Arcand(i+1, arcands_textures[i]));
+	}
 }
 
 function draw() {
     background(20);
-	rotateX(frameCount * 0.01);
-	rotateY(frameCount * 0.02);
-	push();
-	texture(canard1_texture);
-	rotateY(PI);
-	box(160, 240, 8);
-	pop();
-	push();
-	translate(0,0,8);
-	texture(dos_texture);
-	box(160, 240, 8);
-	pop();
-	push();
-	translate(80.5,0,4);
-	rotateY(PI/2);
-	texture(coteVertical_texture);
-	box(16, 240, 0.2);
-	pop();
-	push();
-	translate(-80.5,0,4);
-	rotateY(3 * PI/2);
-	texture(coteVertical_texture);
-	box(16, 240, 0.2);
-	pop();
-	push();
-	translate(0,120.5,4);
-	rotateX(PI/2);
-	texture(coteHorizontal_texture);
-	box(160, 16, 0.2);
-	pop();
-	push();
-	translate(0,-120.5,4);
-	rotateX(3 * PI/2);
-	texture(coteHorizontal_texture);
-	box(160, 16, 0.2);
-	pop();
+	translate(-windowWidth/2, -windowHeight/3);
+	for( let i = 0; i < 9; i ++ ){
+		arcands[i].display((i+1)*170 -100, 200);
+	}
 
 }
 
