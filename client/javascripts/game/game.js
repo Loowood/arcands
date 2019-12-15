cfaf643b059a10a310789de16b46b2404e363cd9
Loowood <lorisg01@yahoo.fr>
2019-12-15 01:05:00 +0100

    Aria + mise en valeur

--- client/javascripts/game/game.js
+++ client/javascripts/game/game.js
@@ -1,5 +1,6 @@
 const queue = new createjs.LoadQueue(true);
 queue.on("fileload", handleFileLoad);
+queue.on("complete", handleComplete);
 // Loading Cards Images
 for (let i = 0; i < 9; i ++) {
 	queue.loadFile({id: "arcand-back", number:i+1, src: "../../assets/images/Dos.png", type: createjs.Types.IMAGE}, false);
@@ -26,7 +27,56 @@ function handleFileLoad (event) {
 	}
 }
 
+function handleComplete (event) {
+	arcands = document.querySelectorAll(".arcand");
+
+	// Aria Labels
+	for ( let i = 0; i < arcands.length; i++ ) {
+		arcands[i].setAttribute("aria-label", "Orbe n°".concat((i+1).toString()) + " Face visible");
+	}
+	// Keyboard event Listeners
+	document.addEventListener('keydown', (event) => {
+		if ( document.querySelector("#characters").contains(document.activeElement) ){
+			let currentIdFocused = parseInt(document.activeElement.id.substr(7, 1));
+			console.log("Current Id Focused", currentIdFocused.toString());
+			if (event.key === "ArrowLeft") {
+				currentIdFocused -= (currentIdFocused > 1)? 1 : 0;
+				document.querySelector("#arcand-".concat(currentIdFocused.toString())).focus();
+			} else if (event.key === "ArrowRight") {
+				currentIdFocused += (currentIdFocused < 9)? 1 : 0;
+				document.querySelector("#arcand-".concat(currentIdFocused.toString())).focus();
+			} else if (event.key === " ") {
+				document.activeElement.click();
+			}
+
+		}else {
+			if (event.key === " " ) {
+				document.querySelector("#arcand-1").focus();
+			}
+		}
+	})
+}
+
 function toggleFlip(event) {
 	console.log(event.target);
+	console.log(event.target.id);
 	console.log(event.target.classList.toggle("flipped"));
+	let idOrbe = /arcand-(\d+)/.exec(event.target.id.toString())[1];
+	if ( event.target.classList.contains("flipped") ) {
+		event.target.setAttribute("aria-label", "Orbe n°".concat(idOrbe) + " Face cachée");
+	} else {
+		event.target.setAttribute("aria-label", "Orbe n°".concat(idOrbe) + " Face visible");
+	}
+}
+
+function toggleFlipTarget(target) {
+	console.log(target);
+	let idOrbe = /arcand-(\d+)/.exec(target.id.toString())[1];
+	console.log(idOrbe);
+	if ( target.classList.contains("flipped") ) {
+		target.setAttribute("aria-label", "Orbe n°".concat(idOrbe) + " Face cachée");
+	} else {
+		target.setAttribute("aria-label", "Orbe n°".concat(idOrbe) + " Face visible");
+	}
+	return target;
 }
